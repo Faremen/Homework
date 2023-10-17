f6fb2a72fed9f4fc750ad3d96f59bfe08112d2e5
faremen <faremanstem@gmail.com>
2023-10-18 00:20:09 +0300

    add solution for task2

---
+++ src/main/java/edu/hw2/task2/Square.java
@@ -0,0 +1,14 @@
+package edu.hw2.task2;
+
+public class Square extends Rectangle {
+
+    @Override
+    public Rectangle setWidth(int width) {
+        return new Rectangle(width, width);
+    }
+
+    @Override
+    public Rectangle setHeight(int height) {
+        return new Rectangle(height, height);
+    }
+}
