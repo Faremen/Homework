4ac07929416ef95cb95f8f7a27f87aa17da27dc4
faremen <faremanstem@gmail.com>
2023-11-03 01:57:28 +0300

    checkStyle fix

--- src/main/java/edu/hw4/AnimalUtils.java
+++ src/main/java/edu/hw4/AnimalUtils.java
@@ -13,6 +13,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@SuppressWarnings("MultipleStringLiterals")
 public class AnimalUtils {
 
     private AnimalUtils() {
--- src/main/java/edu/hw4/ValidationError.java
+++ src/main/java/edu/hw4/ValidationError.java
@@ -9,12 +9,12 @@ public record ValidationError(String field, String message) {
     public static boolean hasError(Animal animal) {
         Objects.requireNonNull(animal);
 
-        return animal.name() == null ||
-            animal.type() == null ||
-            animal.sex() == null ||
-            animal.age() < 0 ||
-            animal.heightInCm() <= 0 ||
-            animal.weightInGram() <= 0;
+        return animal.name() == null
+            || animal.type() == null
+            || animal.sex() == null
+            || animal.age() < 0
+            || animal.heightInCm() <= 0
+            || animal.weightInGram() <= 0;
     }
 
     public static List<ValidationError> getErrors(Animal animal) {
@@ -24,15 +24,20 @@ public record ValidationError(String field, String message) {
 
         if (animal.name() == null) {
             result.add(new ValidationError("name", "name is null"));
-        } if (animal.type() == null) {
+        }
+        if (animal.type() == null) {
             result.add(new ValidationError("type", "type is null"));
-        } if (animal.sex() == null) {
+        }
+        if (animal.sex() == null) {
             result.add(new ValidationError("sex", "sex is null"));
-        } if (animal.age() < 0) {
+        }
+        if (animal.age() < 0) {
             result.add(new ValidationError("age", "age < 0"));
-        } if (animal.heightInCm() <= 0) {
+        }
+        if (animal.heightInCm() <= 0) {
             result.add(new ValidationError("heightInCm", "heightInCm <= 0"));
-        } if (animal.weightInGram() <= 0) {
+        }
+        if (animal.weightInGram() <= 0) {
             result.add(new ValidationError("weightInGram", "weightInGram <= 0"));
         }
 
