40192bdf7995ef2b445635d5d7613bbd91abcaa2
faremen <faremanstem@gmail.com>
2023-11-12 21:33:25 +0300

    checkstyle moment

--- src/main/java/edu/hw5/Task5.java
+++ src/main/java/edu/hw5/Task5.java
@@ -5,9 +5,11 @@ import java.util.regex.Pattern;
 
 public class Task5 {
 
-    public static final Pattern RUS_CAR_NUMBER_PATTERN = Pattern.compile("^[АВЕКМНОРСТУХ](\\d{3})([АВЕКМНОРСТУХ]{2})(\\d){1,3}$");
+    public static final Pattern RUS_CAR_NUMBER_PATTERN =
+        Pattern.compile("^[АВЕКМНОРСТУХ](\\d{3})([АВЕКМНОРСТУХ]{2})(\\d){1,3}$");
 
-    private Task5() {}
+    private Task5() {
+    }
 
     public static boolean checkRusCarNumber(String number) {
         Objects.requireNonNull(number);
