154820a69035f760482e9fad9797f9e5a37a035d
faremen <faremanstem@gmail.com>
2023-11-19 06:20:56 +0300

    test bad fix

--- src/test/java/edu/hw6/task3/FiltersTest.java
+++ src/test/java/edu/hw6/task3/FiltersTest.java
@@ -124,7 +124,7 @@ public class FiltersTest {
         }
 
         // Then
-        assertThat(actual).hasSameElementsAs(expected);
+        assertThat(actual).containsAll(expected);
     }
 
     @Test
