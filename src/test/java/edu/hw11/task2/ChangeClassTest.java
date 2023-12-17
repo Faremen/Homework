574bf0950a3c9007bf958b6aa58738a35b40a2b4
faremen <faremanstem@gmail.com>
2023-12-17 18:00:38 +0300

    add tests for task1-task3

---
+++ src/test/java/edu/hw11/task2/ChangeClassTest.java
@@ -0,0 +1,34 @@
+package edu.hw11.task2;
+
+import edu.hw11.task2.arithmetic.ArithmeticUtils;
+import edu.hw11.task2.arithmetic.SomeArithmeticUtils;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ChangeClassTest {
+    @Test
+    public void sum_ChangeSumToMultiply() {
+        // Given
+        int expected = 10;
+
+        // When
+        ByteBuddyAgent.install();
+        new ByteBuddy()
+            .redefine(ArithmeticUtils.class)
+            .method(ElementMatchers.named("sum"))
+            .intercept(MethodDelegation.to(SomeArithmeticUtils.class))
+            .make()
+            .load(ClassLoader.getSystemClassLoader(), ClassReloadingStrategy.fromInstalledAgent())
+            .getLoaded();
+
+        int actual = ArithmeticUtils.sum(2, 5);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/edu/hw11/task2/arithmetic/SomeArithmeticUtils.java
@@ -0,0 +1,7 @@
+package edu.hw11.task2.arithmetic;
+
+public class SomeArithmeticUtils {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
